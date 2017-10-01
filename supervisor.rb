3ac531a8c9cc30e9f0a6e81cbf919737133dfa71
Peter McCready <petermccready2@gmail.com>
2017-10-01 13:41:38 +0100

    Amends Supervisor initialize and getters

--- supervisor.rb
+++ supervisor.rb
@@ -1,6 +1,14 @@
-class Supervisor
-  def initialize()
+class Supervisor < Employee
 
+  # initialize
+
+  def initialize(first_name, last_name, role, salary, hours_per_day, years_at_company, job_performance)
+    super(first_name, last_name, role, salary, hours_per_day, years_at_company, job_performance)
+    @name = "#{first_name} #{last_name}"
+  end
+
+  def get_name()
+    return @name
   end
 
 end
