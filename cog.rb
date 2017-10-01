e171de887789fe5bad57e723abff2eb81f08eb35
Peter McCready <petermccready2@gmail.com>
2017-10-01 13:30:57 +0100

    Fixes TestCog to use cog.new in setup

--- cog.rb
+++ cog.rb
@@ -3,6 +3,7 @@ require_relative("employee")
 class Cog < Employee
   def initialize(first_name, last_name, role, salary, hours_per_day, years_at_company, job_performance)
     super(first_name, last_name, role, salary, hours_per_day, years_at_company, job_performance)
+    @name = "#{first_name} #{last_name}"
   end
 
   def get_name()
--- specs/cog_spec.rb
+++ specs/cog_spec.rb
@@ -6,11 +6,15 @@ require_relative("../cog")
 class TestCog < MiniTest::Test
 
   def setup()
-    @cog1 = Employee.new("James", "Stewart", "Shelf stacker", 14000.00, 8, 1, 5)
+    @cog1 = Cog.new("James", "Stewart", "Shelf stacker", 14000.00, 8, 1, 5)
+  end
+
+  def test_get_employee_first_name()
+    assert_equal("James", @cog1.get_employee_first_name())
   end
 
   def test_get_name()
-    assert_equal("James", @cog1.get_employee_first_name())
+    assert_equal("James Stewart", @cog1.get_name())
   end
 
 
