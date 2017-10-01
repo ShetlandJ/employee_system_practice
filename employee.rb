d59ded6d9c4cdc8ec9b9a9c2abd737f78a922dec
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-10-01 12:29:33 +0100

    Employee Class getters/setters

--- employee.rb
+++ employee.rb
@@ -1,6 +1,64 @@
 class Employee
-  def initialize()
-
+  def initialize(first_name, last_name, role, salary, hours_per_day, years_at_company, job_performance)
+    @first_name = first_name
+    @last_name = last_name
+    @role = role
+    @salary = salary
+    @hours_per_day = hours_per_day
+    @years_at_company = years_at_company
+    @job_performance = job_performance
   end
 
+  def get_employee_first_name()
+    return @first_name
+  end
+
+  def get_employee_last_name()
+    return @last_name
+  end
+
+  def get_employee_role()
+    return @role
+  end
+
+  def get_employee_salary()
+    return @salary
+  end
+
+
+  def get_employee_working_hours_per_day()
+    return @hours_per_day
+  end
+
+  def get_employee_working_hours_per_week()
+    return @hours_per_day * 5
+  end
+
+  def get_employee_years_at_company()
+    return @years_at_company
+  end
+
+  def get_employee_performance()
+    return @job_performance
+  end
+
+  def promote(new_job_title)
+    @role = new_job_title
+  end
+
+  def increase_salary(new_salary)
+    @salary = new_salary
+  end
+
+  def change_working_hours(time)
+    @hours_per_day += time
+  end
+
+  def increase_years_of_service(year)
+    @years_at_company += year
+  end
+
+  def change_job_performance(annual_review_grade)
+    @job_performance += annual_review_grade
+  end
 end
--- specs/employee_spec.rb
+++ specs/employee_spec.rb
@@ -6,7 +6,65 @@ require_relative("../employee")
 class TestEmployee < MiniTest::Test
 
   def setup()
-    @employee = Employee.new()
+    @employee1 = Employee.new("James", "Stewart", "Shelf stacker", 14000.00, 8, 1, 5)
   end
 
+  def test_get_employee_first_name()
+    assert_equal("James", @employee1.get_employee_first_name)
+  end
+
+  def test_get_employee_last_name()
+    assert_equal("Stewart", @employee1.get_employee_last_name)
+  end
+
+  def test_get_employee_role()
+    assert_equal("Shelf stacker", @employee1.get_employee_role)
+  end
+
+  def test_get_employee_salary()
+    assert_equal(14000.00, @employee1.get_employee_salary)
+  end
+
+  def test_get_working_hours_per_day()
+    assert_equal(8, @employee1.get_employee_working_hours_per_day)
+  end
+
+  def test_get_working_hours_per_week()
+    assert_equal(40, @employee1.get_employee_working_hours_per_week)
+  end
+
+  def test_get_employee_years_at_company()
+    assert_equal(1, @employee1.get_employee_years_at_company)
+  end
+
+
+  def test_get_employee_performance()
+    assert_equal(5, @employee1.get_employee_performance)
+  end
+
+
+  def test_promote()
+    @employee1.promote("Supervisor")
+    assert_equal("Supervisor", @employee1.get_employee_role)
+  end
+
+  def test_increase_salary()
+    @employee1.increase_salary(15000.00)
+    assert_equal(15000.00, @employee1.get_employee_salary)
+  end
+
+  def test_change_workings_hours()
+    @employee1.change_working_hours(1)
+    assert_equal(9, @employee1.get_employee_working_hours_per_day)
+  end
+
+  def test_increase_years_at_company()
+    @employee1.increase_years_of_service(1)
+    assert_equal(2, @employee1.get_employee_years_at_company)
+  end
+
+  def test_change_job_performance()
+    @employee1.change_job_performance(-1)
+    assert_equal(4, @employee1.get_employee_performance)
+  end
 end
